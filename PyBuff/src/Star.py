76735fe309d9fbf52d7ef7600784f8aac91b8a25
Cyuria <wiki.lauer@education.nsw.gov.au>
2021-07-22 14:51:47 +1000

    Update star class to use rect object

--- PyBuff/src/Star.py
+++ PyBuff/src/Star.py
@@ -4,9 +4,6 @@ import rect
 
 # Star class. Defaults to 5 points
 class star(object):
-    def __init__(self, rect, points = 5):
+    def __init__(self, Rect, points = 5):
         self.points = points
-        self.x = rect.x
-        self.y = rect.y
-        self.width = rect.width
-        self.height = rect.height
+        self.Rect = Rect
