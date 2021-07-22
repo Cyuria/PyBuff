a7ec2413f738576b9895cc6f93ae11409e4ca682
Cyuria <wiki.lauer@education.nsw.gov.au>
2021-07-22 14:59:02 +1000

    Add star.draw() function

--- PyBuff/src/Star.py
+++ PyBuff/src/Star.py
@@ -7,3 +7,11 @@ class star(object):
     def __init__(self, Rect, points = 5):
         self.points = points
         self.Rect = Rect
+        if (width < height):
+            self.armLength = width/2
+        else:
+            self.armLength = height/2
+    def draw(self, surface, angle, colour, width = 0):
+        points = []
+
+        pygame.draw.polygon(surface, colour, points, width)
