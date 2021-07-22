60bfbf51d3eeed49396a087551655ec25a6fceea
Cyuria <wiki.lauer@education.nsw.gov.au>
2021-07-22 15:13:32 +1000

    Added TypeError if star.Rect is not a list or a rect

--- PyBuff/src/Star.py
+++ PyBuff/src/Star.py
@@ -2,16 +2,29 @@
 import pygame
 import rect
 
-# Star class. Defaults to 5 points
+# Star class. Defaults to a black 5 pointed star
 class star(object):
-    def __init__(self, Rect, points = 5):
+    def __init__(self, Rect, points = 5, colour = (0, 0, 0)):
         self.points = points
-        self.Rect = Rect
+        self.colour = colour
+
+        # Handle rect variable
+        if type(Rect) == list:
+            self.Rect = rectFromList(Rect)
+        elif type(Rect) != rect:
+            raise TypeError("Variable Rect of class star is not a rect")
+        else:
+            self.Rect = Rect
+
+        # Create ArmLength variable to simplify draw function
         if (width < height):
             self.armLength = width/2
         else:
             self.armLength = height/2
-    def draw(self, surface, angle, colour, width = 0):
-        points = []
-
-        pygame.draw.polygon(surface, colour, points, width)
+    def draw(self, surface, angle, colour = (), width = 0):
+        if colour == ():
+            colour = self.colour
+        coords = []
+        for arm in range(self.points):
+            pass
+        pygame.draw.polygon(surface, colour, coords, width)
